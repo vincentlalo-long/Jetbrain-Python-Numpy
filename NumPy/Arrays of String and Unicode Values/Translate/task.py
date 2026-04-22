8e0a697b1f37eba84a3b310bb89dbc0ad33768eb
vincentlalo-long <nickmoi3056@gmail.com>
2026-04-23 00:38:33 +0700

    feat : math + string unicode

---
+++ NumPy/Arrays of String and Unicode Values/Translate/task.py
@@ -0,0 +1,16 @@
+import string
+
+import numpy as np
+# Add an import here
+
+text = np.array(['Th   Is', '{Is', 'Al+mOst', 'A}', 'nOr m   Al!!!', 'sEnt578EncE', '-nOw'])  # Do not change this.
+
+
+def remove_extra_stuff(txt):
+    return np.char.translate( txt,
+                             str.maketrans('AEIOUY', 'aeiouy', string.punctuation + string.digits + string.whitespace))
+
+
+if __name__ == '__main__':
+    for word in remove_extra_stuff(text):
+        print(word, end=" ")
